939fce02c4c7f1592816ed0ca5c723e81284f86b
fvkramer <krame490@gmail.com>
2018-11-16 17:01:06 -0800

    Write view specs for auth

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'rails_helper'
 
+
 feature 'the signup process' do
   scenario 'has a new user page' do
     visit new_user_url
@@ -32,6 +33,7 @@ feature 'logging in' do
   feature "after sign in" do
     before(:each) do
       visit new_session_url
+      User.create(username: "still_insulted", password: 'vexed420')
       fill_in 'Username', with: 'still_insulted'
       fill_in 'Password', with: 'vexed420'
       click_on 'Moist Meeee'
@@ -45,10 +47,34 @@ feature 'logging in' do
 end
 
 feature 'logging out' do
-  scenario 'begins with a logged out state' do
-    
+  before(:each) do
+    visit new_user_url
+    fill_in 'Username', with: 'insulted'
+    fill_in 'Password', with: 'aggravated'
+    click_on 'Create User'
   end
 
-  scenario 'doesn\'t show username on the homepage after logout'
+  before(:each) do
+    visit new_session_url
+    fill_in 'Username', with: 'still_insulted'
+    fill_in 'Password', with: 'vexed420'
+    click_on 'Moist Meeee'
+  end
+  scenario 'begins with a logged out state' do
+    expect(@user).receive(:log_out!)
+
+
+  end
+
+  before(:each) do
+    visit user_ur
+  end
+  scenario 'doesn\'t show username on the homepage after logout' do
+
+  end
 
 end
+# 
+# @user = User.create(username: "Kambooka", password: "lethologica")
+# visit user_url(@user)
+# expect(page).to have_content 'Moist Me More'
